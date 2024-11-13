66e215e0d9a97fb062b234a8c0f8a9f056dd4db9
hyojun <hyojun@gmail.com>
2024-11-13 14:35:03 +0900

    chore: run server on background

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ ps x | grep uvicorn | grep -v grep | awk '{print $1}' | xargs kill -9
 
 # start server
 . ./venv/bin/activate
-uvicorn main:app --host=0.0.0.0
+nohup uvicorn main:app --host=0.0.0.0 > ./output.log 2>&1 &
